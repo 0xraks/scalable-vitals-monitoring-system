95e6c38b2769a84fec0f933ebfbf1e8a12b5cf7d
0xraks <rakshith152@gmail.com>
2021-06-21 03:24:23 +0530

    added docker file

--- WebApp/app.js
+++ WebApp/app.js
@@ -10,7 +10,8 @@ const { Schema } = require("influx/lib/src/schema");
 const axios = require("axios");
 const fetch = require("node-fetch");
 const { result } = require("lodash");
-
+const PORT = 3000 ; 
+const HOST = '0.0.0.0';
 const app = express();
 const server = require("http").createServer(app);
 const { Server } = require("socket.io");
@@ -138,8 +139,8 @@ app.get("/api/:id", (request, response) => {
 //     res.redirect("/dashboard");
 // });
 
-server.listen(3000, function () {
-    console.log("Server started on port 3000");
+server.listen(PORT,HOST, function () {
+    console.log(`Server started on port  ${PORT}`);
 });
 
 // //------------
